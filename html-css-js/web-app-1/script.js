5d558f35cc18503621ad5b85cdcddf25dd5176f9
Ramya-0710 <ramyasri.alla14@gmail.com>
2026-01-10 10:58:33 +0530

    commit

---
+++ html-css-js/web-app-1/script.js
@@ -0,0 +1,6 @@
+function dispMsg() {
+    //alert("Hello! Welcome to the Web App 1");
+    // document.getElementById("lblMsg").innerHTML = "Hello! Welcome to the Web App 1";
+    alert(document.getElementById("txt").value);
+
+}
