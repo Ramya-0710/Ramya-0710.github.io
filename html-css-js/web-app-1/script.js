5f68c353905d185b940f470d0cfe0d8bf2caf978
Ramya-0710 <ramyasri.alla14@gmail.com>
2026-01-10 11:02:57 +0530

    update

--- html-css-js/web-app-1/script.js
+++ html-css-js/web-app-1/script.js
@@ -1,6 +1,7 @@
 function dispMsg() {
     //alert("Hello! Welcome to the Web App 1");
     // document.getElementById("lblMsg").innerHTML = "Hello! Welcome to the Web App 1";
-    alert(document.getElementById("txt").value);
-
+    //alert(document.getElementById("txt").value);
+    lblMessage.innerHTML=document.getElementById("txt").value;
+    
 }
