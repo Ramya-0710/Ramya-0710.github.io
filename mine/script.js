2b547a75861dd255df9ffbc2d8cfde5a0539a5cf
Ramya-0710 <ramyasri.alla14@gmail.com>
2026-01-12 02:27:47 +0530

    update

--- mine/script.js
+++ mine/script.js
@@ -4,12 +4,15 @@ function toggleMenu() {
 
   if (menu.style.display === "block") {
     menu.style.display = "none";
-    btn.innerHTML = "&#9776;"; // ☰ menu icon
+    btn.innerHTML = "&#9776;"; // ☰
+     btn.style.fontSize = "30px";
   } else {
     menu.style.display = "block";
-    btn.innerHTML = "&times;"; // ✖ close icon
+    btn.innerHTML = "&times;"; // ✖
+   btn.style.fontSize = "40px";
   }
 }
+
 function about(){
     alert("This is the About Me section.");
 }
